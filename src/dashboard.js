068ab8c2b161069a5717e72fdbbc29f90f70d700
applesAreTasty123 <80722925+applesAreTasty123@users.noreply.github.com>
2021-12-29 21:51:34 -0800

    Updated on 12/29/2021
    Changed <code>window.location.href</code>

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,6 +1,6 @@
 document.getElementById("person").innerHTML = localStorage.getItem("Username");
 
 document.getElementById("logOut").addEventListener("click", function() {
-    window.location.href = "http://localhost:5500/Login%20page";
+    window.location.href = "/Login%20page";
     localStorage.setItem("IsLoged", false);
 });
