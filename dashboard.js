fe57dbafee19e7fd5b2f9f96f3885d6ba4ddc404
applesAreTasty123 <80722925+applesAreTasty123@users.noreply.github.com>
2021-12-29 21:46:31 -0800

    Delete dashboard.js
    moved dashboard.js

--- dashboard.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("person").innerHTML = localStorage.getItem("Username");
-
-document.getElementById("logOut").addEventListener("click", function() {
-    window.location.href = "http://localhost:5500/Login%20page";
-    localStorage.setItem("IsLoged", false);
-});
